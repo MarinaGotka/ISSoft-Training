4ac8ad58b5de7e0d4ecb5c22a5c6464639c3b521
MarinaGotka <marinagotka@coherentsolutions.com>
2019-01-22 15:00:53 +0300

    Task1 is added

---
+++ QAAutomationTraining/TUT.by/Tests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace TUT.by
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Init()
+        {
+            HomePage.GoToUrl("https://www.tut.by/");
+        }
+
+        [TearDown]
+        public void Dispose()
+        {
+            LoginPopUp.Logout();
+            HomePage.driver.Close();
+        }
+
+        [Test]
+        public void LoginTutBy()
+        {
+            string username = "seleniumtests@tut.by";
+            string password = "123456789zxcvbn";
+            string userNameAfterLogin = "Selenium Test";
+
+            HomePage.LogInClick();
+            LoginPopUp.Login(username, password);
+
+            Assert.True(LoginPopUp.LoginAs(userNameAfterLogin));
+        }
+    }
+}
