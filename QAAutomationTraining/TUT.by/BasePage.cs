ce240764165ac8c3dbcac83fbe4ca4aecc9e7276
MarinaGotka <marinagotka@coherentsolutions.com>
2019-02-01 15:50:25 +0300

    Add polling interval to explicit waiter

--- QAAutomationTraining/TUT.by/BasePage.cs
+++ QAAutomationTraining/TUT.by/BasePage.cs
@@ -6,21 +6,23 @@ namespace TUT.by
 {
     public class BasePage
     {
+        private readonly IClock clock = new SystemClock();
+
         public void WaitUntilDisplayed(IWebDriver driver, By locator)
         {
-            var element = new WebDriverWait(driver, TimeSpan.FromSeconds(15)).Until(condition =>
-            {
-                try
-                {
-                    var elementToBeDisplayed = driver.FindElement(locator);
+            var element = new WebDriverWait(clock, driver, TimeSpan.FromSeconds(15), TimeSpan.FromMilliseconds(500)).Until(condition =>
+              {
+                  try
+                  {
+                      var elementToBeDisplayed = driver.FindElement(locator);
 
-                    return elementToBeDisplayed.Displayed;
-                }
-                catch (Exception)
-                {
-                    return false;
-                }
-            });
+                      return elementToBeDisplayed.Displayed;
+                  }
+                  catch (Exception)
+                  {
+                      return false;
+                  }
+              });
         }
     }
 }
