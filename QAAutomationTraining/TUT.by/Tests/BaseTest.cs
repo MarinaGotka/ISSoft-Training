95dd099d6f9cb07db62e94ebf2c939efa0aa4e6d
MarinaGotka <marinagotka@coherentsolutions.com>
2019-03-04 16:57:26 +0300

    Added Remote driver

--- QAAutomationTraining/TUT.by/Tests/BaseTest.cs
+++ QAAutomationTraining/TUT.by/Tests/BaseTest.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using System;
 using System.IO;
 
@@ -15,6 +16,9 @@ namespace TUT.by.Tests
         public void SetUp()
         {
             driver = new ChromeDriver();
+            ChromeOptions Options = new ChromeOptions();
+            Options.PlatformName = "WIN10";
+            driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), Options.ToCapabilities(), TimeSpan.FromSeconds(25000));
             driver.Navigate().GoToUrl(Url);
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15); // Implicit waiter for WebDriver. 
